a4cd968f1c57619da2dae0732ceae6d7873c56f3
TheKunalSen <ksen959595@gmail.com>
2023-10-03 00:15:13 +0530

    first commit

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ const camera = new THREE.PerspectiveCamera(
   10000
 );
 camera.position.z = 1;
-camera.lookAt(0,0,2000);
-
+camera.lookAt(0,2000,0);
+camera.position.set(0,-200,0)
 
 const scene = new THREE.Scene();
 const canvas = document.querySelector('#jihad');
@@ -31,14 +31,56 @@ const material = new THREE.MeshBasicMaterial({
 
 let mats= [];
 let part_num = 5000;
-for(let i = 0; i <part_num;i++) {
+for(let i = 0; i <part_num;i+=1) {
   const particles = new THREE.SphereGeometry(5, 12, 12); 
   const particalMaterial = new THREE.MeshBasicMaterial( {color: "#fff"} ); 
   var cube = new THREE.Mesh( particles, particalMaterial ); 
 mats.push(cube);
-  const x = Math.floor(Math.random() * 600) - 300;
-  const y = Math.floor(Math.random() * 650) - 350;
-  const z = Math.floor(Math.random() * 190) - 90;
+
+// const angle = Math.random() * Math.PI * 2;
+  
+//   // Generate a random radius between 0 and a certain maximum radius (e.g., 100 units)
+//   const maxRadius = 100;
+//   const radius = Math.random() * maxRadius;
+  
+//   // Convert polar coordinates to Cartesian coordinates
+//   const x = radius * Math.cos(angle);
+//   const y = radius * Math.sin(angle);
+  
+//   // Set a constant z-coordinate if you want a flat disk
+//   const z = 0;
+
+
+// const theta = Math.random() * Math.PI * 2;  // Azimuth angle (0 to 2π)
+// const phi = Math.acos(2 * Math.random() - 1);  // Inclination angle (0 to π)
+// const radius = 100;  // Adjust the radius as needed
+
+// // Generate random points on the hollow sphere
+
+// const x = radius * Math.sin(phi) * Math.cos(theta);
+// const y = radius * Math.sin(phi) * Math.sin(theta);
+// const z = radius * Math.cos(phi);
+
+
+  // const x = Math.floor(Math.random() * 600) - 300;
+  // const y = Math.floor(Math.random() * 250) - 150;
+  // const z = Math.floor(Math.random() * 190) - 90;
+//   const radius = Math.random() * 100; // Adjust the radius as needed
+// const theta = Math.random() * Math.PI * 2; // Random angle around the vertical axis
+// const phi = Math.random() * Math.PI; // Random angle from the top (zenith) to the bottom (nadir) of the sphere
+// const x = radius * Math.sin(phi) * Math.cos(theta);
+// const y = radius * Math.sin(phi) * Math.sin(theta);
+// const z = radius * Math.cos(phi);
+
+const a = 100;
+const t = Math.random() * 2 * Math.PI;
+    
+// Calculate 'x' and 'y' based on the Lemniscate equations
+const x = (a * Math.cos(t)) / (1 + Math.sin(t) ** 2);
+const y = (a * Math.sin(t) * Math.cos(t)) / (1 + Math.sin(t) ** 2);
+
+// Generate random values for 'z' within your desired range
+const z = Math.floor(Math.random() * 190) - 90;
   cube.position.set(5,1,10)
  gsap.to(cube.position, {
 x:x,
@@ -73,7 +115,7 @@ console.log(mats)
 
 
 
-camera.position.set(0,0,-200)
+
 const initialCameraPosition = new THREE.Vector3(0, 0, 5); // Set your initial camera position
 const targetCameraPosition = new THREE.Vector3(0, 2, 5); // Set your target camera position
 // 5-40
@@ -89,20 +131,28 @@ let jihad = true;
 document.querySelector('button').addEventListener('click', () =>{
   if (jihad) {
     gsap.to(camera.position, {
-     z:200,
+     y:200,
  
   
       duration: animationDuration,
-      delay:5,
+      // delay:5,
       ease: 'easeInOut',
       // onUpdate: updateCameraPosition,
       onComplete: () => {
         // Animation completed
       },
     });
+     gsap.to(camera, {
+        fov: 200,
+        duration: 10, // Animation duration in seconds
+        onUpdate: function () {
+            camera.updateProjectionMatrix(); // Update the camera's projection matrix
+        },
+    });
+
     mats.forEach((mat) =>{
       gsap.to(mat.scale,{
-        z:2,
+        y:2,
        delay:-1,
         duration:10,
         ease: 'easeInOut',
@@ -119,7 +169,7 @@ document.querySelector('button').addEventListener('click', () =>{
   else {
     gsap.to(camera.position, {
      
-      z: -500,
+      y: -500,
       ease: 'easeInOut',
       duration: animationDuration,
       // delay:2,
@@ -128,9 +178,16 @@ document.querySelector('button').addEventListener('click', () =>{
         // Animation completed
       },
     });
+        gsap.to(camera, {
+        fov: 10,
+        duration: 10, // Animation duration in seconds
+        onUpdate: function () {
+            camera.updateProjectionMatrix(); // Update the camera's projection matrix
+        },
+    });
     mats.forEach((mat) =>{
       gsap.to(mat.scale,{
-        z:0,
+        y:0,
        delay:-1,
         duration:10,
         ease: 'easeInOut',
